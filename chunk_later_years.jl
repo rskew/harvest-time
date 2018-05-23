c208b9409271763b8f783164c1891a2e961e83c1
Rowan Skewes <rowan.skewes@gmail.com>
2018-05-23 21:09:18 +1000

    Integer relaxation for later years and chunkify
    Integer relaxation currently not working.
    
    Chunkify is halfway there using extra variables,
    next commit will jsut change the age constraints
    for later ages.

--- chunk_later_years.jl
+++ chunk_later_years.jl
@@ -12,40 +12,28 @@ minimum_yield_per_year = 500_000
 maximum_yield_per_year = 1_500_000
 n_properties = 199
 n_years = 8
+n_future_chunks = 4
+n_chunk_spacing = 3
 replanting_cost = 1000
 time_limit = 120 # seconds
 
 
 properties = 1:n_properties
-#initial_ages = ones(length(properties),1)
 initial_ages = initialAge
-#slopes = [1,2,3,4,5,1,2,3,4,5,1,2,3,4,5,1,2,3,4,5]
-#plateau_years = 4*ones(length(properties),1)
 property_age = 1:length(density[1,:])
-years_to_plan = 1:n_years
+years = 1:n_years
+years_plus_chunks = 1:(n_years+n_future_chunks)
+future_chunks = 1:n_future_chunks
 M = 100000000
 
-years_to_plan = 1:1
+years = 1:1
 minimum_yield_per_year = 5
 maximum_yield_per_year = 100
-
-#yields = zeros(length(properties),length(property_age))
-#for p in properties
-#    for pa in property_age
-#        current_age = initial_ages[p] + pa - 1
-#        if current_age < plateau_years[pa]
-#            yields[p,pa] = slopes[p] * current_age
-#        else
-#            yields[p,pa] = slopes[p] * plateau_years[p]
-#        end
-#    end
-#end
-
 
 
 function print_harvests(harvests)
     for p in properties
-        for t in years_to_plan
+        for t in years
             print("$(Int(round(getvalue(harvests[p,t])))) ")
         end
         println()
@@ -66,90 +54,116 @@ end
 m = Model(solver=GurobiSolver(TimeLimit=time_limit))
 # 'harvest' is 1 for a property for a given year if that property is
 # harvested that year
-@variable(m, harvest[properties,years_to_plan], Bin)
+@variable(m, harvest[properties,years], Bin)
+@variable(m, future_harvest[properties,future_chunks], Bin)
 
-# 'age' gives the number of years_to_plan since the previous harvest of a property
-@variable(m, age[properties,years_to_plan], Int)
+# 'age' gives the number of years since the previous harvest of a property
+@variable(m, age[properties,years], Int)
+@variable(m, future_age[properties,future_chunks], Int)
 
-# HARVEST_age stores the age of a property on the years_to_plan it is harvested.
-@variable(m, harvest_age[properties,years_to_plan], Int)
+# harvest_age stores the age of a property on the years it is harvested.
+@variable(m, harvest_age[properties,years], Int)
+@variable(m, future_harvest_age[properties,future_chunks], Int)
 
 # Linearise by parts the age vs yield function.
 # Each 'harvest_age[p,t]' has its own linearised curve variables.
 # The 'y's from the lecture notes:
-@variable(m, y[properties, years_to_plan, property_age-1], Bin)
+@variable(m, y[properties, years_plus_chunks, property_age-1], Bin)
 # The lambdas from the lecture notes:
-@variable(m, 0 <= lambda[properties, years_to_plan, property_age] <= 1)
+@variable(m, 0 <= lambda[properties, years_plus_chunks, property_age] <= 1)
 
 # Maximise profit: balance yield against replanting cost
 @objective(m, Max,
            sum(lambda[p,t,pa] * yields[p,pa] * netReturns[p]
                for p in properties,
-               t in years_to_plan,
+               t in years_plus_chunks,
                pa in property_age)
            - sum(harvest[p,t] * replanting_cost * area[p]
                  for p in properties,
-                 t in years_to_plan))
+                 t in years)
+           - sum(future_harvest[p,c] * replanting_cost * area[p]
+                 for p in properties,
+                 t in future_chunks))
 
 # Integer variables >= 0
-@constraint(m, [p in properties, t in years_to_plan],
+@constraint(m, [p in properties, t in years],
             age[p,t] >= 0)
-@constraint(m, [p in properties, t in years_to_plan],
+@constraint(m, [p in properties, t in years],
+            future_age[p,t] >= 0)
+@constraint(m, [p in properties, t in years],
             harvest_age[p,t] >= 0)
+@constraint(m, [p in properties, t in years],
+            future_harvest_age[p,t] >= 0)
 
 # Linearise by parts constraints
-@constraint(m, [p in properties, t in years_to_plan],
+@constraint(m, [p in properties, t in years],
             harvest_age[p,t] == sum(lambda[p,t,pa] * pa
                                     for pa in property_age))
-@constraint(m, [p in properties, t in years_to_plan],
+@constraint(m, [p in properties, c in future_chunks],
+            future_harvest_age[p,c] == sum(lambda[p,c+n_years,pa] * pa
+                                           for pa in property_age))
+
+@constraint(m, [p in properties, t in years],
             sum(lambda[p,t,pa] for pa in property_age) == harvest[p,t])
-@constraint(m, [p in properties, t in years_to_plan],
+@constraint(m, [p in properties, c in future_chunks],
+            sum(lambda[p,c+n_years,pa] for pa in property_age) == future_harvest[p,c])
+
+@constraint(m, [p in properties, t in years_plus_chunks],
             sum(y[p,t,pa] for pa in property_age[1:end-1]) == 1)
-@constraint(m, [p in properties, t in years_to_plan],
+
+@constraint(m, [p in properties, t in years_plus_chunks],
             lambda[p,t,1] <= y[p,t,1])
-@constraint(m, [p in properties, t in years_to_plan, pa in property_age[1:end-2]],
+
+@constraint(m, [p in properties, t in years_plus_chunks, pa in property_age[1:end-2]],
             lambda[p,t,pa+1] <= y[p,t,pa] + y[p,t,pa+1])
-@constraint(m, [p in properties, t in years_to_plan],
+
+@constraint(m, [p in properties, t in years_plus_chunks],
             lambda[p,t,length(property_age)] <= y[p,t,length(property_age)-1])
 
-# harvest_age is zero for years_to_plan where there is no harvest
-@constraint(m, [p in properties, t in years_to_plan],
+# harvest_age is zero for years where there is no harvest
+@constraint(m, [p in properties, t in years],
             harvest_age[p,t] <= M * harvest[p,t])
+@constraint(m, [p in properties, c in future_chunks],
+            future_harvest_age[p,c] <= M * future_harvest[p,c])
 # harvest_age is the age of the property when it is harvested.
 # To maximise the objective, the solver will automatically push
-# harvest_age up to the age of the property on harvest years_to_plan.
-@constraint(m, [p in properties, t in years_to_plan],
+# harvest_age up to the age of the property on harvest years.
+@constraint(m, [p in properties, t in years],
             harvest_age[p,t] <= age[p,t])
+@constraint(m, [p in properties, c in future_chunks],
+            future_harvest_age[p,c] <= future_age[p,c])
 # Force 'harvest_age[p,t]' to be 'age[p,t]' when harvest[p,t]
-@constraint(m, [p in properties, t in years_to_plan],
+@constraint(m, [p in properties, t in years],
             harvest_age[p,t] >= age[p,t] - M * (1 - harvest[p,t]))
+@constraint(m, [p in properties, c in chunks],
+            future_harvest_age[p,t] >= future_age[p,t] - M * (1 - harvest[p,t]))
 
 # Assign initial ages
 @constraint(m, [p in properties],
             age[p,1] == initial_ages[p] + 1)
 # 'age' must be 1 for the year after a harvest. For any other year, 'age' must
 # be the previous age +1.
-@constraint(m, [p in properties, t in years_to_plan],
+@constraint(m, [p in properties, t in years],
             age[p, t] >= 1)
-@constraint(m, [p in properties, prev_t in years_to_plan, next_t in years_to_plan; prev_t+1 == next_t],
+@constraint(m, [p in properties, prev_t in years, next_t in years; prev_t+1 == next_t],
             age[p, next_t] <= age[p, prev_t] + 1)
 
-@constraint(m, [p in properties, prev_t in years_to_plan, next_t in years_to_plan; prev_t+1 == next_t],
+@constraint(m, [p in properties, prev_t in years, next_t in years; prev_t+1 == next_t],
             age[p, next_t] <= 1 + M * (1 - harvest[p,prev_t]))
 
-@constraint(m, [p in properties, prev_t in years_to_plan, next_t in years_to_plan; prev_t+1 == next_t],
+@constraint(m, [p in properties, prev_t in years, next_t in years; prev_t+1 == next_t],
             age[p, next_t] >= age[p, prev_t] + 1 - M * harvest[p,prev_t])
 
 
 # Must provide a minimum quantity per year.
-@constraint(m, [t in years_to_plan],
+@constraint(m, [t in years],
             sum(lambda[p,t,pa] * yields[p,pa]
                 for p in properties,
                 pa in property_age)
             >= minimum_yield_per_year)
 
 # The amount of product that can be processed is limited.
-@constraint(m, [t in years_to_plan],
+@constraint(m, [t in years],
             sum(lambda[p,t,pa] * yields[p,pa]
                 for p in properties,
                 pa in property_age)
@@ -157,34 +171,15 @@ m = Model(solver=GurobiSolver(TimeLimit=time_limit))
 
 solve(m)
 
-#println("harvest[p,t]:")
-#print_harvests(harvest)
-#println("age[p,t]:")
-#print_harvests(age)
 println("harvest_ages[p,t]:")
 print_harvests(harvest_age)
 println("yields[p,t]:")
 print_yields(yields)
 println("yields per year:")
-for t in years_to_plan
+for t in years
     print("$(sum(getvalue(lambda[p,t,pa]*yields[p,pa]) for p in properties, pa in property_age)) ")
 end
 println()
-#println("lambda:")
-#for pa in property_age
-#    for t in years_to_plan
-#        print("$(getvalue(lambda[length(properties),t,pa])) ")
-#    end
-#    println()
-#end
-#
-#println("y:")
-#for pa in property_age[1:end-1]
-#    for t in years_to_plan
-#        print("$(Int(round(getvalue(y[length(properties),t,pa])))) ")
-#    end
-#    println()
-#end
 
 
 println("Total cost $(getobjectivevalue(m))")
--- relaxation_integer_later_years.jl
+++ relaxation_integer_later_years.jl
@@ -3,33 +3,36 @@ using Gurobi
 
 include("read_data.jl")
 
-#filename = ARGS[1]
-
-# Dummy data for model development
-
 properties = 1:length(initialAge)
-minimum_yield_per_year = 500_000
-maximum_yield_per_year = 1_500_000
-n_properties = 199
-n_years_total = 10
+#minimum_yield_per_year = 500_000
+#maximum_yield_per_year = 1_500_000
+#n_properties = 199
+minimum_yield_per_year = 50_0
+maximum_yield_per_year = 150_00000
+n_properties = 20
 n_years_int = 5
 n_years_real = 5
-replanting_cost = 10
+#n_real_harvests = n_years_real
+n_real_harvests = 7
+n_years_total = n_years_int + n_years_real
+replanting_cost = 1000
 time_limit = 30 # seconds
 
 
 properties = 1:n_properties
 initial_ages = initialAge
 property_age = 1:length(yields[1,:])
-years_to_plan_total = 1:n_years_total
-years_to_plan_int = 1:n_years_int
-years_to_plan_real = 1:n_years_real
+real_harvests = 1:n_real_harvests
+years_int = 1:n_years_int
+years_real = 1:n_years_real
+years_total = 1:n_years_total
+harvests = 1:(n_years_int+n_real_harvests)
 M = 100000000
 
 
 function print_harvests(harvests)
     for p in properties
-        for t in years_to_plan_int
+        for t in years_int
             print("$(Int(round(getvalue(harvests[p,t])))) ")
         end
         println()
@@ -48,170 +51,288 @@ end
 
 m = Model(solver=GurobiSolver(TimeLimit=time_limit))
 
-# 'harvest' is 1 for a propertt for a given year if that property is
+# 'harvest' is 1 for a property for a given year if that property is
 # harvested that year
-@variable(m, harvest[properties,years_to_plan_int], Bin)
+@variable(m, harvest[properties,years_int], Bin)
 
-# 'age' gives the number of years_to_plan since the previous harvest of a property
-@variable(m, age[properties,years_to_plan_int], Int)
+# 'age' gives the number of years since the previous harvest of a property
+@variable(m, age[properties,years_int], Int)
 
-# HARVEST_age stores the age of a property on the years_to_plan it is harvested.
-@variable(m, harvest_age[properties,years_to_plan_int], Int)
+# HARVEST_age stores the age of a property on the years it is harvested.
+@variable(m, harvest_age[properties,years_int], Int)
 
-########################
 ########################
 # Relaxation: add a set of real valued harvest age variables for each property
 # for years after the integer planning period
 # eg integer harvest ages and years up to 5 years of planning,
 # then relaxed real values for ages and harvest dates from that to 30 years
 
-@variable(m,real_harvest_age[properties,years_to_plan_real])
+# Whether or not to use this real harvest variable
+@variable(m, real_harvest[properties,real_harvests], Bin)
+
+# Which real valued time in years this harvest happens
+@variable(m, n_years_int <= real_harvest_year[properties,real_harvests] <= n_years_total)
+
+# The age of the property at the harvest time in years
+@variable(m, real_harvest_age[properties,real_harvests])
 
 
-# for each real harvest year, need to add a constraint that it's the sum of two lambdas*t
+# Bunch the real harvests at the start of the array.
+# When they stop being used, there can be no more harvests to the right
+@constraint(m, [p in properties, r in real_harvests ; r < n_real_harvests],
+            real_harvest[p,r+1] <= real_harvest[p,r])
 
+# Real harvests are listed in consecutive order
+@constraint(m, [p in properties, r in real_harvests ; r < n_real_harvests],
+            real_harvest_year[p,r+1] >= real_harvest_year[p,r])
 
-
-# harvest_age is zero for years_to_plan where there is no harvest
-@constraint(m, [p in properties, t in years_to_plan],
-            harvest_age[p,t] <= M * harvest[p,t])
-# harvest_age is the age of the property when it is harvested.
-# To maximise the objective, the solver will automatically push
-# harvest_age up to the age of the property on harvest years_to_plan.
-@constraint(m, [p in properties, t in years_to_plan],
-            harvest_age[p,t] <= age[p,t])
-# Force 'harvest_age[p,t]' to be 'age[p,t]' when harvest[p,t]
-@constraint(m, [p in properties, t in years_to_plan],
-            harvest_age[p,t] >= age[p,t] - M * (1 - harvest[p,t]))
-
-# Assign initial ages
+# First real harvest age is the age at the end of the integer period plus the time since then
 @constraint(m, [p in properties],
-            age[p,1] == initial_ages[p] + 1)
-# 'age' must be 1 for the year after a harvest. For any other year, 'age' must
-# be the previous age +1.
-@constraint(m, [p in properties, t in years_to_plan],
-            age[p, t] >= 1)
-@constraint(m, [p in properties, prev_t in years_to_plan, next_t in years_to_plan; prev_t+1 == next_t],
-            age[p, next_t] <= age[p, prev_t] + 1)
+            real_harvest_age[p,1] == age[p,n_years_int] + (real_harvest_year[p,1] - n_years_int))
+# Calculate real_harvest_age from real_harvest_year
+@constraint(m, [p in properties, r in real_harvests ; r >= 2],
+            real_harvest_age[p,r] == real_harvest_year[p,r] - real_harvest_year[p,r-1])
 
-@constraint(m, [p in properties, prev_t in years_to_plan, next_t in years_to_plan; prev_t+1 == next_t],
-            age[p, next_t] <= 1 + M * (1 - harvest[p,prev_t]))
-
-@constraint(m, [p in properties, prev_t in years_to_plan, next_t in years_to_plan; prev_t+1 == next_t],
-            age[p, next_t] >= age[p, prev_t] + 1 - M * harvest[p,prev_t])
 
 ########################
-########################
-
 # Linearise by parts the age vs yield function.
 # Each 'harvest_age[p,t]' has its own linearised curve variables.
 # The 'y's from the lecture notes:
-@variable(m, y[properties, years_to_plan_total, property_age-1], Bin)
+@variable(m, y[properties, harvests, property_age[1:end-1]], Bin)
 # The lambdas from the lecture notes:
-@variable(m, 0 <= lambda[properties, years_to_plan_total, property_age] <= 1)
+@variable(m, 0 <= lambda[properties, harvests, property_age] <= 1)
 
-# Maximise profit: balance yield against replanting cost
-@objective(m, Max,
-           sum(lambda[p,t,pa] * yields[p,pa]
-               for p in properties,
-               t in years_to_plan,
-               pa in property_age)
-           - sum(harvest[p,t] * replanting_cost
-                 for p in properties,
-                 t in years_to_plan))
-
-# Integer variables >= 0
-@constraint(m, [p in properties, t in years_to_plan],
-            age[p,t] >= 0)
-@constraint(m, [p in properties, t in years_to_plan],
-            harvest_age[p,t] >= 0)
-
-# Linearise by parts constraints
-@constraint(m, [p in properties, t in years_to_plan],
+# (Int) harvest age interpolates two lambdas
+@constraint(m, [p in properties, t in years_int],
             harvest_age[p,t] == sum(lambda[p,t,pa] * pa
                                     for pa in property_age))
-@constraint(m, [p in properties, t in years_to_plan],
+# (Real) harvest age interpolated from lambdas
+@constraint(m, [p in properties, r in real_harvests],
+            real_harvest_age[p,r] == sum(lambda[p,r+n_years_int,pa] * pa
+                                    for pa in property_age))
+
+# (Int) Lambdas must sum to 1 to be valid interpolation
+@constraint(m, [p in properties, t in years_int],
             sum(lambda[p,t,pa] for pa in property_age) == harvest[p,t])
-@constraint(m, [p in properties, t in years_to_plan],
+# (Real) Lambdas must sum to 1 to be valid interpolation
+@constraint(m, [p in properties, r in real_harvests],
+            sum(lambda[p,r+n_years_int,pa] for pa in property_age) == real_harvest[p,r])
+
+# (Int) Only choose 1 pair of consecutive lambdas using y variable to signal which pair
+@constraint(m, [p in properties, t in harvests],
             sum(y[p,t,pa] for pa in property_age[1:end-1]) == 1)
-@constraint(m, [p in properties, t in years_to_plan],
+# (Real) Only choose 1 pair of consecutive lambdas using y variable to signal which pair
+@constraint(m, [p in properties, r in real_harvests],
+            sum(y[p,r+n_years_int,pa] for pa in property_age[1:end-1]) == real_harvest[p,r])
+# Left endpoint
+@constraint(m, [p in properties, t in harvests],
             lambda[p,t,1] <= y[p,t,1])
-@constraint(m, [p in properties, t in years_to_plan, pa in property_age[1:end-2]],
+# Midpoints
+@constraint(m, [p in properties, t in harvests, pa in property_age[1:end-2]],
             lambda[p,t,pa+1] <= y[p,t,pa] + y[p,t,pa+1])
-@constraint(m, [p in properties, t in years_to_plan],
+# Right endpoint
+@constraint(m, [p in properties, t in harvests],
             lambda[p,t,length(property_age)] <= y[p,t,length(property_age)-1])
 
-# harvest_age is zero for years_to_plan where there is no harvest
-@constraint(m, [p in properties, t in years_to_plan],
-            harvest_age[p,t] <= M * harvest[p,t])
-# harvest_age is the age of the property when it is harvested.
-# To maximise the objective, the solver will automatically push
-# harvest_age up to the age of the property on harvest years_to_plan.
-@constraint(m, [p in properties, t in years_to_plan],
-            harvest_age[p,t] <= age[p,t])
-# Force 'harvest_age[p,t]' to be 'age[p,t]' when harvest[p,t]
-@constraint(m, [p in properties, t in years_to_plan],
-            harvest_age[p,t] >= age[p,t] - M * (1 - harvest[p,t]))
+
+########################
+# Need var to keep track of the discrete year that a property was harvested
+# so that max/min harvest constraints can be enforced each year.
+# This is done with another linearisation by parts type method, with lambda
+# and y vars allowing a real harvest age to be allocated to a bucket of
+# harvests that happen in a certain year.
+@variable(m, real_harvest_year_bucket[properties,real_harvests,years_real[1:end-1]], Bin)
+@variable(m, 0 <= real_harvest_year_lambda[properties,real_harvests,years_real] <= 1)
+
+# harvest year interpolated from lambdas
+@constraint(m, [p in properties, r in real_harvests],
+            real_harvest_year[p,r] == sum(real_harvest_year_lambda[p,r,t] * (t+n_years_int)
+                                          for t in years_real))
+
+# Lambdas must sum to 1 to be valid interpolation, or 0 if not harvested
+@constraint(m, [p in properties, r in real_harvests],
+            sum(real_harvest_year_lambda[p,r,t] for t in years_real) == real_harvest[p,r])
+
+# Only choose 1 pair of consecutive lambdas using bucket variable to signal which pair
+@constraint(m, [p in properties, r in real_harvests],
+            sum(real_harvest_year_bucket[p,r,t] for t in years_real[1:end-1]) == real_harvest[p,r])
+# Left endpoint
+@constraint(m, [p in properties, r in real_harvests],
+            real_harvest_year_lambda[p,r,1] <= real_harvest_year_bucket[p,r,1])
+# Midpoints
+@constraint(m, [p in properties, r in real_harvests, t in years_real[1:end-2]],
+            real_harvest_year_lambda[p,r,t+1] <= real_harvest_year_bucket[p,r,t] + real_harvest_year_bucket[p,r,t+1])
+# Right endpoint
+@constraint(m, [p in properties, r in real_harvests],
+            real_harvest_year_lambda[p,r,n_years_real] <= real_harvest_year_bucket[p,r,n_years_real-1])
+
+# Need a variable to hold the yield harvested from a property if it was
+# harvested in a particular year bucket, so it can be summed for enforcing
+# constraints on yield per whole year
+@variable(m, real_yield_if_harvested_on_year[properties,real_harvests,years_real])
+
+@constraint(m, [p in properties, r in real_harvests, t in years_real[1:end-1]],
+            real_yield_if_harvested_on_year[p,r,t] <= M * real_harvest_year_bucket[p,r,t])
+
+@constraint(m, [p in properties, r in real_harvests, t in years_real[1:end-1]],
+            real_yield_if_harvested_on_year[p,r,t] <= sum(lambda[p,r+n_years_int,pa] * yields[p,pa]
+                                                          for pa in property_age))
+
+@constraint(m, [p in properties, r in real_harvests, t in years_real[1:end-1]],
+            real_yield_if_harvested_on_year[p,r,t] >= sum(lambda[p,r+n_years_int,pa] * yields[p,pa]
+                                                          for pa in property_age)
+                                                      - M * (1 - real_harvest_year_bucket[p,r,t]))
+
+##################################################
+# (Int) age constraints
 
 # Assign initial ages
 @constraint(m, [p in properties],
             age[p,1] == initial_ages[p] + 1)
 # 'age' must be 1 for the year after a harvest. For any other year, 'age' must
 # be the previous age +1.
-@constraint(m, [p in properties, t in years_to_plan],
+@constraint(m, [p in properties, t in years_int],
             age[p, t] >= 1)
-@constraint(m, [p in properties, prev_t in years_to_plan, next_t in years_to_plan; prev_t+1 == next_t],
+@constraint(m, [p in properties, prev_t in years_int, next_t in years_int; prev_t+1 == next_t],
             age[p, next_t] <= age[p, prev_t] + 1)
 
-@constraint(m, [p in properties, prev_t in years_to_plan, next_t in years_to_plan; prev_t+1 == next_t],
+@constraint(m, [p in properties, prev_t in years_int, next_t in years_int; prev_t+1 == next_t],
             age[p, next_t] <= 1 + M * (1 - harvest[p,prev_t]))
 
-@constraint(m, [p in properties, prev_t in years_to_plan, next_t in years_to_plan; prev_t+1 == next_t],
+@constraint(m, [p in properties, prev_t in years_int, next_t in years_int; prev_t+1 == next_t],
             age[p, next_t] >= age[p, prev_t] + 1 - M * harvest[p,prev_t])
 
+# harvest_age is zero for years where there is no harvest
+@constraint(m, [p in properties, t in years_int],
+            harvest_age[p,t] <= M * harvest[p,t])
+# harvest_age is the age of the property when it is harvested.
+# To maximise the objective, the solver will automatically push
+# harvest_age up to the age of the property on harvest years.
+@constraint(m, [p in properties, t in years_int],
+            harvest_age[p,t] <= age[p,t])
+# Force 'harvest_age[p,t]' to be 'age[p,t]' when harvest[p,t]
+@constraint(m, [p in properties, t in years_int],
+            harvest_age[p,t] >= age[p,t] - M * (1 - harvest[p,t]))
 
+
+##################################################
 # Must provide a minimum quantity per year.
-@constraint(m, [t in years_to_plan],
+@constraint(m, [t in years_int],
             sum(lambda[p,t,pa] * yields[p,pa]
                 for p in properties,
                 pa in property_age)
             >= minimum_yield_per_year)
+# Real
+@constraint(m, [t in years_real],
+            sum(real_yield_if_harvested_on_year[p,r,t]
+                for p in properties,
+                r in real_harvests)
+            >= minimum_yield_per_year)
 
 # The amount of product that can be processed is limited.
-@constraint(m, [t in years_to_plan],
+@constraint(m, [t in years_int],
             sum(lambda[p,t,pa] * yields[p,pa]
                 for p in properties,
                 pa in property_age)
             <= maximum_yield_per_year)
+# Real
+@constraint(m, [t in years_real],
+            sum(real_yield_if_harvested_on_year[p,r,t]
+                for p in properties,
+                r in real_harvests)
+            <= maximum_yield_per_year)
+
+
+# Maximise profit: balance yield against replanting cost
+@objective(m, Max,
+           sum(lambda[p,t,pa] * yields[p,pa] * netReturns[p]
+               for p in properties,
+               t in years_int,
+               pa in property_age)
+           + sum(lambda[p,r+n_years_int,pa] * yields[p,pa] * netReturns[p]
+                 for p in properties,
+                 r in real_harvests,
+                 pa in property_age)
+           - sum(harvest[p,t] * replanting_cost * area[p]
+                 for p in properties,
+                 t in years_int)
+           - sum(real_harvest[p,r] * replanting_cost * area[p]
+                 for p in properties,
+                 r in real_harvests))
+
+# Integer variables >= 0
+@constraint(m, [p in properties, t in years_int],
+            age[p,t] >= 0)
+@constraint(m, [p in properties, t in years_int],
+            harvest_age[p,t] >= 0)
+@constraint(m, [p in properties, r in real_harvests],
+            real_harvest_age[p,r] >= 0)
+@constraint(m, [p in properties, r in real_harvests, t in years_real],
+            real_yield_if_harvested_on_year[p,r,t] >= 0)
+
 
 solve(m)
 
-#println("harvest[p,t]:")
-#print_harvests(harvest)
-#println("age[p,t]:")
-#print_harvests(age)
 println("harvest_ages[p,t]:")
 print_harvests(harvest_age)
 println("yields[p,t]:")
 print_yields(yields)
 println("yields per year:")
-for t in years_to_plan
+for t in years_int
     print("$(sum(getvalue(lambda[p,t,pa]*yields[p,pa]) for p in properties, pa in property_age)) ")
 end
 println()
-#println("lambda:")
-#for pa in property_age
-#    for t in years_to_plan
-#        print("$(getvalue(lambda[length(properties),t,pa])) ")
-#    end
+
+println("real_harvest:")
+for p in properties
+    for r in real_harvests
+        print("$(Int(round(getvalue(real_harvest[p,r])))) ")
+    end
+    println()
+end
+
+println("real_harvest_year:")
+for p in properties
+    for r in real_harvests
+        print("$(getvalue(real_harvest_year[p,r])) ")
+    end
+    println()
+end
+
+println("real_harvest_age:")
+for p in properties
+    for r in real_harvests
+        print("$(getvalue(real_harvest_age[p,r])) ")
+    end
+    println()
+end
+
+println("real_harvest_year_lambda:")
+for t in years_real
+    for r in real_harvests
+        print("$(getvalue(real_harvest_year_lambda[n_properties,r,t])) ")
+    end
+    println()
+end
+
+println("real_harvest_year_bucket:")
+for t in years_real[1:end-1]
+    for r in real_harvests
+        print("$(Int(round(getvalue(real_harvest_year_bucket[n_properties,r,t])))) ")
+    end
+    println()
+end
+
+println("real_yield_if_harvested_on_year:")
+#for p in properties
+p = n_properties
+    for t in years_real
+        for r in real_harvests
+            print("$(getvalue(real_yield_if_harvested_on_year[p,r,t])) ")
+        end
+        println()
+    end
 #    println()
-#end
-#
-#println("y:")
-#for pa in property_age[1:end-1]
-#    for t in years_to_plan
-#        print("$(Int(round(getvalue(y[length(properties),t,pa])))) ")
-#    end
 #    println()
 #end
 
